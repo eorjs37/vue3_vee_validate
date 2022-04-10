164e5e2d2a48a2d7ae5002193ebfdd0d3a08e9cc
CHOI DAE GEON <chleorjs37@gmail.com>
2022-04-10 11:13:19 +0900

    커밋

---
+++ src/utils/veevalidate.js
@@ -0,0 +1,17 @@
+import { configure, defineRule} from 'vee-validate';
+import AllRules from '@vee-validate/rules';
+import { localize } from '@vee-validate/i18n';
+import en from '@vee-validate/i18n/dist/locale/en.json';
+import ko from '@vee-validate/i18n/dist/locale/ko.json';
+
+// Install all rules
+Object.keys(AllRules).forEach(rule => {
+    defineRule(rule, AllRules[rule]);
+});
+
+configure({
+  generateMessage: localize({
+    en,
+    ko
+  }),
+});
